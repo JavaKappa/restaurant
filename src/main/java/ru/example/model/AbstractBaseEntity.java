d6e60c6809bf57e8cc4d754d3e89eb313480181a
JavaKappa <nextnation7@gmail.com>
2020-05-17 23:34:28 +0300

    change Sequence generator logic

--- src/main/java/ru/example/model/AbstractBaseEntity.java
+++ src/main/java/ru/example/model/AbstractBaseEntity.java
@@ -9,10 +9,11 @@ import javax.persistence.*;
 @MappedSuperclass
 
 public abstract class AbstractBaseEntity {
+    private static final int START_SEQ = 10000;
     @Id
-    @SequenceGenerator(name = "id")
+    @SequenceGenerator(name = "hibernate_sequence", sequenceName = "hibernate_sequence", allocationSize = 1, initialValue = START_SEQ)
 //    //@Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hibernate_sequence")
     protected Integer id;
 
     public AbstractBaseEntity(Integer id) {
